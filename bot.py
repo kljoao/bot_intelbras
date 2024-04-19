72358996e2b73c071e9f4cb6899af5b55cbb8e48
kljoao <joao.ramos@infocrerj.local>
2024-04-19 17:15:59 -0300

    release

--- bot.py
+++ bot.py
@@ -4,12 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from selenium.common.exceptions import StaleElementReferenceException
 import smtplib
 import time
 import customtkinter
 
 janela = customtkinter.CTk()
-janela.geometry("400x200")
+janela.geometry("400x350")
 
 # Configurações de e-mail
 email_user = ''
@@ -18,9 +19,6 @@ email_password = ''
 # Definindo as variáveis como globais
 global email_alerta
 global nome_usuario
-global email_enviado  # Adicione esta linha
-
-email_enviado = False  # Inicialize a variável email_enviado como False
 
 def click():
     global email_alerta
@@ -28,7 +26,7 @@ def click():
 
     # Atribuir os valores dos campos de entrada às variáveis globais
     email_alerta = email_alerta_entry.get()
-    nome_usuario = nome_usuario_entry.get()
+    nome_usuario = nome_usuario_entry.get().split(", ")
 
     # Fechar a janela
     janela.destroy()
@@ -36,6 +34,19 @@ def click():
     # Imprimir os valores preenchidos
     print("E-mail Receptivo:", email_alerta)
     print("Usuário em Alerta:", nome_usuario)
+
+# Configurações E-mail remetente - INICIO
+configuracoes = customtkinter.CTkLabel(janela, text="Configurações E-mail Remetente")
+configuracoes.pack(padx=10, pady=10)
+
+email_remetente_entry = customtkinter.CTkEntry(janela, placeholder_text="E-mail remetente", width=250)
+email_remetente_entry.pack(padx=10, pady=10)
+
+email_remetente_senha_entry = customtkinter.CTkEntry(janela, placeholder_text="Senha do E-mail Remetente", width=250)
+email_remetente_senha_entry.pack(padx=10, pady=10)
+# Configurações E-mail remetente - FIM
+
+# Configurações Destinatário - INICIO
 
 texto = customtkinter.CTkLabel(janela, text="Inserir Usuário Alerta")
 texto.pack(padx=10, pady=10)
@@ -55,13 +66,7 @@ email_destinatario = email_alerta
 
 usuarioAlerta = nome_usuario
 
-def enviar_email():
-    global email_enviado
-
-    # Se o e-mail já foi enviado, saia da função
-    if email_enviado:
-        return
-
+def enviar_email(nome_acesso):
     msg = MIMEMultipart()
     msg['From'] = email_user
     msg['To'] = email_destinatario
@@ -71,19 +76,22 @@ def enviar_email():
     <html>
     <body>
         <p>Olá, FULANO</p>
-        <p>Verificamos que o usuário {usuarioAlerta} teve acesso o serviço de biometria</p>
+        <p>Verificamos que o usuário {nome_acesso} teve acesso o serviço de biometria</p>
     </body>
     <html>
     """
 
     msg.attach(MIMEText(corpo_email, 'html'))
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(email_user, email_password)
-        server.sendmail(email_user, email_destinatario, msg.as_string())
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(email_user, email_password)
+            server.sendmail(email_user, email_destinatario, msg.as_string())
+    except smtplib.SMTPException as e:
+        print("Erro ao enviar o e-mail:", e)
+        return  # Se ocorrer um erro, saia da função
 
-    email_enviado = True  # Defina email_enviado como True após o envio do e-mail
 
 # Configurações do WebDriver
 options = webdriver.ChromeOptions()
@@ -108,17 +116,49 @@ driver.find_element(By.XPATH, '/html/body/app-root/app-login/div/mat-sidenav-con
 modal = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-home/div/app-popup-primeiros-passos/p-dialog/div/div[1]/a')))
 modal.click()
 
+time.sleep(20)
+
+# Armazenar os valores iniciais da data, hora e nome do card
+data_anterior = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[1]').text
+hora_anterior = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[2]').text
+nome_anterior = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[3]/div/span[1]').text
+
 
 while True:
     try:
-        # Verificar se o nome do usuário está presente
-        nome_xpath = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[3]/div/span[1]')))
-        nome = nome_xpath.text
-        if nome == nome_usuario and not email_enviado:  # Adicione esta linha
-            enviar_email()
+        # Verificar se os elementos ainda estão presentes na página
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[1]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[2]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[3]/div/span[1]')))
+        
+        # Capturar os novos valores da data, hora e nome do card
+        nova_data = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[1]').text
+        nova_hora = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[1]/div[2]').text
+        novo_nome = driver.find_element(By.XPATH, '/html/body/app-root/app-home/div/app-menu/div/mat-sidenav-container/mat-sidenav[2]/div/app-sidebar/div/mat-sidenav-container/mat-sidenav/div/div/div[1]/div/div[3]/div/span[1]').text
+        
+        # Dividir a string de novo_nome em uma lista de nomes
+        novo_usuario_lista = novo_nome.split(", ")
+        
+        # Verificar se algum nome na lista corresponde ao nome específico
+        if any(nome in novo_usuario_lista for nome in nome_usuario):
+            # Verificar se houve alguma alteração na data ou na hora do card
+            if nova_data != data_anterior or nova_hora != hora_anterior:
+                for nome in novo_usuario_lista:
+                    if nome in nome_usuario:
+                        enviar_email(nome)  # Se houver alteração, enviar o e-mail de alerta
+                        break
+                
+                # Atualizar os valores anteriores para os novos valores
+                data_anterior = nova_data
+                hora_anterior = nova_hora
         
         # Aguardar um intervalo de tempo antes de recarregar a página
-        time.sleep(10)  # Aguardar 10 segundos antes de verificar novamente
+        time.sleep(3)  # Aguardar 3 segundos antes de verificar novamente
+    except StaleElementReferenceException:
+        # Se um elemento se tornou obsoleto, atualize os valores anteriores e continue
+        data_anterior = nova_data
+        hora_anterior = nova_hora
+        continue
     except Exception as e:
         print("Erro:", e)
         print("Reiniciando o WebDriver...")
